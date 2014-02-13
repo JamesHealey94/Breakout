34f8fdfcdd210c065d964c724431a536fe492b6c
JamesHealey94 <jameshealey1994@gmail.com>
2014-02-13 03:56:00 +0000

    Basic JFrame set up

--- Breakout/src/com/gmail/jameshealey1994/breakout/gui/ScoresGUI.java
+++ Breakout/src/com/gmail/jameshealey1994/breakout/gui/ScoresGUI.java
@@ -1,6 +1,11 @@
 package com.gmail.jameshealey1994.breakout.gui;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
 
 /**
  * Scores Menu GUI for Breakout Game.
@@ -8,5 +13,30 @@ import javax.swing.JFrame;
  * @author JamesHealey94 <jameshealey1994.gmail.com>
  */
 public class ScoresGUI extends JFrame {
-    //
+
+    /**
+     * JPanel used to display the scores.
+     */
+    private final JPanel scoresPanel;
+
+    /**
+     * Constructor - Sets up JFrame.
+     */
+    public ScoresGUI() {
+        this.setTitle("Breakout - Scores");
+        this.setBackground(Color.LIGHT_GRAY);
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        //this.setIconImage(null); // TODO create Icon
+        this.setLayout(new BorderLayout());
+
+        scoresPanel = new GamePanel();
+        this.add(scoresPanel, BorderLayout.CENTER);
+        this.setResizable(false);
+        this.pack();
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(640, 480);
+    }
 }
